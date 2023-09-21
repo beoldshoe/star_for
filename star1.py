ddb3b6aa5d7be2871f73491d47ffc71ded3e1d48
hyunsoo Kim <howeve18@gmail.com>
2023-09-21 21:37:23 +0900

    V2: 왼쪽 삼각형 리팩토링
    반복문으로 구현

--- star1.py
+++ star1.py
@@ -1,5 +1,6 @@
-print("    *")
-print("   **")
-print("  ***")
-print(" ****")
-print("*****")
+for i in range(1,6):
+    for j in range(5-i):
+        print('',end="")
+    for j in range(i):
+        print('*',end="")
+    print('')
